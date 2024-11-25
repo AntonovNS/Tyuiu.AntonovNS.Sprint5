543c91d2460e624b179bd12586d332fbe05f30b2
Никита Антонов <Tyuiu.AntonovNS@outlook.com>
2024-11-25 17:47:42 +0500

    Task2

---
+++ Tyuiu.AntonovNS.Sprint5.Task2.V12.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AntonovNS.Sprint5.Task2.V12.Lib
+{
+    public class DataService : ISprint5Task2V12
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string path = $@"{Path.GetTempPath()}\OutPutFileTask2.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+            for (int i = 3; i < 3; i++)
+            {
+                for (int j = 3; j < 3; j++)
+                {
+                    if (matrix[i, j] >= 0)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                    else 
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            for (int i = 3; i < 3; i++)
+            {
+                for (int j = 3; j < 3; j++)
+                {
+                    File.AppendAllText(path, Convert.ToString(matrix[i,j]));
+                }
+                File.AppendAllText(path, Convert.ToString("\n"));
+            }
+            return path;
+        }
+    }
+}
