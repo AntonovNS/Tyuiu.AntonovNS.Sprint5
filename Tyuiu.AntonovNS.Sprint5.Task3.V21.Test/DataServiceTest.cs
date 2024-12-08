6792e911915bf6c3151df8fd5001d2402b790754
Никита Антонов <Tyuiu.AntonovNS@outlook.com>
2024-12-08 17:59:36 +0500

    Task3

---
+++ Tyuiu.AntonovNS.Sprint5.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using Tyuiu.AntonovNS.Sprint5.Task3.V21.Lib;
+namespace Tyuiu.AntonovNS.Sprint5.Task3.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+            string path = Path.GetTempFileName();
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            int x = 3;
+            string path1 = ds.SaveToFileTextData(x);
+            Assert.AreEqual(path, path1);
+        }
+    }
+}
