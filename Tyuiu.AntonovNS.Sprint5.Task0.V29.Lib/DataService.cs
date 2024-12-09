cccc15b77decfb2388022a52d5e2057b2ab20cd5
Никита Антонов <Tyuiu.AntonovNS@outlook.com>
2024-12-09 19:53:50 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.AntonovNS.Sprint5.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AntonovNS.Sprint5.Task0.V29.Lib
+{
+    public class DataService : ISprint5Task0V29
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Path.GetTempPath()}\OutPutFileTask0.txt";
+            double res = Math.Round(6.1 * Math.Pow(x, 3) + 0.23 * Math.Pow(x, 2) + 1.04 * x, 3);
+            File.WriteAllText(path, Convert.ToString(res));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.AntonovNS.Sprint5.Task1.V1.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AntonovNS.Sprint5.Task1.V1.Lib
+{
+    public class DataService : ISprint5Task1V1
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = $@"{Path.GetTempPath()}\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                double res = Math.Round((5 * i + 2.5) / (Math.Sin(i) + 2) + 2 * i + 2, 2);
+                if (res == 0)
+                {
+                    File.AppendAllText(path, Convert.ToString(0));
+                    File.AppendAllText(path, Convert.ToString("\n"));
+                }
+                else
+                {
+                    File.AppendAllText(path, Convert.ToString(res));
+                    File.AppendAllText(path, Convert.ToString("\n"));
+                }
+            }
+            return path;
+        }
+    }
+}
