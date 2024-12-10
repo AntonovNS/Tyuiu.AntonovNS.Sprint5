4148513da4220daa4d0b5a5d6f2eb79dd436d7ca
Никита Антонов <Tyuiu.AntonovNS@outlook.com>
2024-12-10 14:10:16 +0500

    Task7

---
+++ Tyuiu.AntonovNS.Sprint5.Task7.V26.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AntonovNS.Sprint5.Task7.V26.Lib
+{
+    public class DataService : ISprint5Task7V26
+    {
+        public string LoadDataAndSave(string path)
+        {
+            double res = 0;
+            int s = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (double.TryParse(line, out double number) && number >= 0)
+                    {
+                        res = res + number;
+                        s++;
+                    }
+
+                }
+            }
+            return "2";
+        }
+    }
+}
